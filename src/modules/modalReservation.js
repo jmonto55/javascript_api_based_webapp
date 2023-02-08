e9f5cbd6792202cf674c79d4783c310538cf09e4
Sajib Arafat Siddiqui <sajib.arafat@nrbglobalbank.com>
2023-02-08 18:14:26 +0600

    Added ingredients and measure of cooking in dom

--- src/modules/modalReservation.js
+++ src/modules/modalReservation.js
@@ -4,10 +4,12 @@ export const modalReservation = async e =>{
     e.preventDefault();
     const id = e.target.id;
     const data = await getDogById(id);
-    console.log(data.meals[0].strMeal)
+    console.log(Object.keys(data.meals[0]).filter((k=>k.startsWith('strIngredient'))).forEach(elt=>{if(data.meals[0][elt]){console.log(data.meals[0][elt])}}))
     const modalContainer = document.getElementsByClassName('modal')[0];
     modalContainer.style.display="block"
     const modalContent=document.getElementsByClassName('reservation')[0];
+    const ingredients=Object.keys(data.meals[0]).filter((k=>k.startsWith('strIngredient')))
+    const measures=Object.keys(data.meals[0]).filter((k=>k.startsWith('strMeasure')))
     modalContent.innerHTML = `
     <span id="close">&times</span>
     <img id='dog_img' src=${data.meals[0].strMealThumb} alt=${data.meals[0].strMeal
@@ -16,7 +18,10 @@ export const modalReservation = async e =>{
     <p>${data.meals[0].strInstructions}</p>
     <ul id='dog_spec'>
     <li>Area:${data.meals[0].strArea}</li>
+
     <li>Category:${data .meals[0].strCategory}</li>
+    ${ingredients.map((elt,n)=>`<li>${data.meals[0][elt] }:${data.meals[0][measures[n]]}</li>`).join('')}
+
     </ul>
 
     <form id="reservation_data">
