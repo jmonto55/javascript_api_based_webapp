012bb2b1bb90464e92db8402deeac3f5de934cf8
SFarida <faridagueleu@gmail.com>
2023-02-09 22:02:42 +0100

    Remove comment

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import './style.css';
 import './modules/tools.js';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'bootstrap';
 
-// document.getElementById('myModal').style.display = 'none'
 document.getElementById('closeModal').addEventListener('click', () => {
   document.getElementById('myModal').style.display = 'none';
 });
