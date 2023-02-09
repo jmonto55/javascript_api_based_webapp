5c210cbd9fa3ba47b976409bc3023332198d8067
SFarida <faridagueleu@gmail.com>
2023-02-09 21:55:53 +0100

    Fix issue raised by reviewer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import './style.css';
 import './modules/tools.js';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'bootstrap';
 
 // document.getElementById('myModal').style.display = 'none'
 document.getElementById('closeModal').addEventListener('click', () => {
