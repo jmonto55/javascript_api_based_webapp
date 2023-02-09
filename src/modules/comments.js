688d30d45b99e80b6ea11cba5310c9113fa9d685
jmonto55 <jmonto55@gmail.com>
2023-02-09 18:51:18 -0500

    Add minor style changes on comments modal

--- src/modules/comments.js
+++ src/modules/comments.js
@@ -41,14 +41,14 @@ const displayPopup = (meal, comment = null) => {
           <div class="comment_container" id="comment_container">
              ${ul ? ul.innerHTML : ''}
           </div>
-          <h5>Add a comment</h5>
+          <h4>Add a comment</h4>
           <form id="form" class="comment_form">
               <input type="hidden" value="${meal.idMeal}" id="mealId"/>
               <div>
                   <input type="text" id="name" name="name" placeholder="Your Name" required>
               </div>
               <div>
-                  <textarea id="comment" name="w3review" rows="4" cols="50" placeholder="Your insight" required></textarea><br>
+                  <textarea id="comment" name="w3review" rows="4" placeholder="Your insight" required></textarea><br>
               </div>
               <div class="buttons_container">
               <button class="btn btn-outline-dark">Submit</button>
